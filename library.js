09c3bddf18f5b54896ac5cee5b530fba0d6ec0f7
AnEvilPenguin <dave.lewis@squaredup.com>
2024-02-29 15:58:20 +0000

    feat: basic book display

--- library.js
+++ library.js
@@ -1,12 +1,6 @@
-const myLibrary = [
-    new Book(
-        'Zeus Grants Stupid Wishes: A No-Bullshit Guide to World Mythology',
-        'Cory O\'Brien',
-        304,
-        true,
-        'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1356120567i/15808302.jpg'
-    )
-];
+const library = document.querySelector('.library');
+
+const myLibrary = [];
 
 function Book(title, author, pages, read, coverImgUrl) {
     this.title = title;
@@ -16,12 +10,68 @@ function Book(title, author, pages, read, coverImgUrl) {
     this.coverImgUrl = coverImgUrl;
 }
 
+function createElement(type, className, id) {
+    const element = document.createElement(type);
+
+    element.classList.add(className);
+    element.id = id;
+
+    return element;
+}
+
+
+function displayBook({title, author, pages, read, coverImgUrl}, id) {
+    const card = createElement('div', 'card', id);
+
+    const titleElement = createElement('h2', 'title');
+    titleElement.textContent = title;
+
+    card.appendChild(titleElement);
+
+    const content = createElement('div', 'content');
+
+    const authorElement = createElement('p', 'author');
+    authorElement.textContent = author;
+
+    const pagesElement = createElement('p', 'pages');
+    pagesElement.textContent = `${pages} pages`;
+
+    content.appendChild(authorElement);
+    content.appendChild(pagesElement);
+
+    const details = createElement('div', 'details');
+    details.appendChild(content);
+
+    card.appendChild(details);
+
+    // TODO figure out how to SVG nicely.
+
+    const img = createElement('img', 'cover');
+    img.setAttribute('src', coverImgUrl ?? 'assets/cover-not-found.jpg');
+    img.setAttribute('alt', 'Book cover');
+    
+    card.appendChild(img);
+
+    library.appendChild(card);
+}
+
 function addBookToLibrary(title, author, pages, read, coverImgUrl) {
     const newBook = new Book(title, author, pages, read, coverImgUrl);
     myLibrary.push(newBook);
+    displayBook(newBook, myLibrary.length);
     // TODO display book on page
         // possibly delete all cards from library and rebuild from array?
         // simply append new book?
 }
+
+// initial test book
+addBookToLibrary(
+    'Zeus Grants Stupid Wishes: A No-Bullshit Guide to World Mythology',
+    'Cory O\'Brien',
+    304,
+    true,
+    'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1356120567i/15808302.jpg'
+);
+
 
 // TODO allow delete of book from array.
